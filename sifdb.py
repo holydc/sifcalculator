eace26b98848ed227fa6c2ed9e063bd0b1232f52
holydc <holydc@gmail.com>
2013-12-04 17:14:03 +0800

    store parsed card data to database

--- sifdb.py
+++ sifdb.py
@@ -12,8 +12,38 @@ class SifParser(html.parser.HTMLParser):
     self._tag = [None]
     self._table = None
     self._record = None
+    self._records = None
     self._data = None
     self._rowspan = None
+
+    # database
+    self._db = sqlite3.connect('sif.db')
+    c = self._db.cursor()
+    c.execute('DROP TABLE IF EXISTS sif')
+    c.execute(
+        'CREATE TABLE sif('
+        '_no INTEGER NOT NULL PRIMARY KEY, '
+        'img VARCHAR(255), '
+        'name VARCHAR(255) NOT NULL, '
+        'type VARCHAR(7) NOT NULL, '
+        'stamina INTEGER, '
+        'smile INTEGER, '
+        'pure INTEGER, '
+        'cool INTEGER, '
+        'max_stamina INTEGER, '
+        'max_smile INTEGER, '
+        'max_pure INTEGER, '
+        'max_cool INTEGER, '
+        'final_max_stamina INTEGER, '
+        'final_max_smile INTEGER, '
+        'final_max_pure INTEGER, '
+        'final_max_cool INTEGER, '
+        'skill VARCHAR(255), '
+        'center_skill VARCHAR(255), '
+        'rarity VARCHAR(7) NOT NULL'
+        ')')
+    self._db.commit()
+    c.close()
 
   def appendData(self, data):
     if (len(self._data) == 0):
@@ -30,57 +60,93 @@ class SifParser(html.parser.HTMLParser):
         rowspan = None
         self._rowspan.pop(len(self._record) - 1)
 
+  def insertRecord(self):
+    query = None
+    if (self._table == 'N'):
+      query = (
+          'INSERT INTO sif (_no, img, name, type, stamina, smile, pure, cool, final_max_stamina, final_max_smile, final_max_pure, final_max_cool, rarity) '
+          'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+    else:
+      query = (
+          'INSERT INTO sif '
+          'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+    c = self._db.cursor()
+    for record in self._records:
+      c.execute(query, record)
+    self._db.commit()
+    c.close()
+
+  def handle_starttable(self):
+    self._rowspan = {}
+    self._records = []
+
+  def handle_endtable(self):
+    self.insertRecord()
+    self._table = None
+    self._rowspan = None
+    self._records = None
+
+  def handle_starttr(self):
+    self._record = []
+
+  def handle_endtr(self):
+    # check trailing rowspan
+    self.appendRowspan()
+
+    # the first column should be number
+    if (self._record[0] != None) and (self._record[0].isdigit()):
+      self._record.append(self._table)
+      self._records.append(tuple(self._record))
+    self._record = None
+
+  def handle_starttd(self, attrs):
+    self._data = ''
+    colspan = None
+
+    # append data for rowspan
+    self.appendRowspan()
+
+    # check colspan and rowspan
+    for attr in attrs:
+      if (attr[0] == 'colspan'):
+        colspan = int(attr[1])
+      elif (attr[0] == 'rowspan'):
+        self._rowspan[len(self._record)] = [int(attr[1]) - 1, None]
+    if (colspan != None):
+      for i in range(1, colspan):
+        self._record.append(None)
+
+  def handle_endtd(self):
+    # record for rowspan
+    if len(self._record) in self._rowspan.keys():
+      rowspan = self._rowspan[len(self._record)]
+      if (rowspan[1] == None):
+        rowspan[1] = self._data
+
+    if (len(self._data) == 0):
+      self._data = None
+    self._record.append(self._data)
+    self._data = None
+
   def handle_starttag(self, tag, attrs):
     self._tag.append(tag)
     if (self._table != None):
       if (tag == 'table'):
-        self._rowspan = {}
-        # TODO: create table
+        self.handle_starttable()
       if (tag == 'tr'):
-        self._record = []
+        self.handle_starttr()
       elif (tag == 'td'):
-        self._data = ''
-        colspan = None
-
-        # append data for rowspan
-        self.appendRowspan()
-
-        # check colspan and rowspan
-        for attr in attrs:
-          if (attr[0] == 'colspan'):
-            colspan = int(attr[1])
-          elif (attr[0] == 'rowspan'):
-            self._rowspan[len(self._record)] = [int(attr[1]) - 1, None]
-        if (colspan != None):
-          for i in range(1, colspan):
-            self._record.append('')
+        self.handle_starttd(attrs)
 
   def handle_endtag(self, tag):
     self._tag.pop()
     if (self._table != None):
       if (tag == 'table'):
-        self._table = None
-        self._rowspan = None
+        self.handle_endtable()
       elif (tag == 'tr'):
-        # check trailing rowspan
-        self.appendRowspan()
-
-        # the first column should be number
-        if self._record[0].isdigit():
-          self._record.append(self._table)
-          # TODO: insert record into database
-          print(len(self._record))
-          print(self._record)
-        self._record = None
+        self.handle_endtr()
       elif (tag == 'td'):
-        # record for rowspan
-        if len(self._record) in self._rowspan.keys():
-          rowspan = self._rowspan[len(self._record)]
-          if (rowspan[1] == None):
-            rowspan[1] = self._data
-
-        self._record.append(self._data)
-        self._data = None
+        self.handle_endtd()
 
   def handle_startendtag(self, tag, attrs):
     if (self._record != None) and (tag == 'img'):
@@ -94,7 +160,6 @@ class SifParser(html.parser.HTMLParser):
       m = re.match(r'\[(\w+)\]部員', data_)
       if (m != None):
         self._table = m.groups()[0]
-        print('h2: %s' % self._table)
     elif (self._record != None):
       tags = ('td', 'a', 'strong', 'span')
       if self._tag[-1] in tags:
